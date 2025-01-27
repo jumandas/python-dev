750c98b3f0dd2b77dfdd0d0f705e9e1e86bab83c
Juman Das <dasjuman9@gmail.com>
2025-01-27 23:21:44 +0530

    CS-50 Initial Commit

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,1 +1,42 @@
+#hello world 
 print("Hello World")
+
+#Input with return values (variables)
+name = input("Whats your name?")
+
+#Input with return values (variables) Add all the function in the same line
+name = input("Whats your name?").strip().title()
+
+#remove whitespace from string
+name = name.strip()
+
+#Capitalize user's name (only first letter of first word)
+name = name.capitalize()
+
+#Capitalize user's fullname
+name = name.title()
+
+#use combined function
+name = name.strip().title()
+
+
+print("Hello, " + name)
+
+#print on same line - other way-1
+print("Hello, ", end="")
+print(name)
+
+#print on same line - other way-2
+print("hello,",name,sep="")
+
+#print on same line - other way-3
+print("hello,", name)
+
+
+#print with double quotes ,escape string
+print('hello, "friend"')
+print("hello , \"friend\"")
+
+#special string
+print(f"hello,{name}")
+
