3837589ef93a20a35e90f780dbb96d77e643b1ee
jumandas <64024146+jumandas@users.noreply.github.com>
2025-01-28 19:12:32 +0530

    Created SecondProgram.py
    CS-50 Harvard Course

---
+++ SecondProgram.py
@@ -0,0 +1,33 @@
+#addition of 2 numbers - int
+x=input("Whats x?")
+y=input("Whats y?")
+
+z= int(x) + int(y)
+
+print(z)
+
+#nested addition - float
+a=int(input("a"))
+b=int(input("b"))
+
+print(a+b)
+
+#float
+c=float(input("c"))
+d=float(input("d"))
+
+print(c+d)
+
+#round (round figure)
+z= round(c+d)
+
+# format the number with a comma
+print(f"{z:,}")
+
+
+#divide 
+e=float(input("e"))
+f=float(input("f"))
+g = e/f
+
+print(g)
