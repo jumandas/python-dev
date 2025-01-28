01affe25905520eaffb16ab3950e20a591cf2364
jumandas <64024146+jumandas@users.noreply.github.com>
2025-01-28 20:10:36 +0530

    Update Conditionals.py

--- Conditionals.py
+++ Conditionals.py
@@ -1,6 +1,9 @@
 #conditional statements
 # > ,>= ,< ,<= ,== ,!=
 
+#if , elif 
+
+
 #if statement 
 
 x=int(input("Whats the first number?"))
@@ -11,4 +14,17 @@ if x < y:
 if x > y:
     print("x is greater than y")
 if x == y:
+    print("x is equal to y")
+
+
+#elif statement 
+
+x=int(input("Whats the first number?"))
+y=int(input("Whats the second number?"))
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+elif x == y:
     print("x is equal to y")
