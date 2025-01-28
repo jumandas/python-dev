b142e6e255653adede7438bea260bd6795b57992
jumandas <64024146+jumandas@users.noreply.github.com>
2025-01-28 20:06:18 +0530

    if statement in python

---
+++ Conditionals.py
@@ -0,0 +1,14 @@
+#conditional statements
+# > ,>= ,< ,<= ,== ,!=
+
+#if statement 
+
+x=int(input("Whats the first number?"))
+y=int(input("Whats the second number?"))
+
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x is equal to y")
