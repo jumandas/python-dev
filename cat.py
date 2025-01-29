9f3909d5938d5eeeb8ca58f898d5c0e7ebc012ff
jumandas <dasjuman9@gmail.com>
2025-01-29 21:42:54 +0530

    loops

---
+++ cat.py
@@ -0,0 +1,24 @@
+#loops 
+
+#while loop
+
+i= 0
+while i < 3:
+    print("meow")
+    i +=  1
+
+print("-------------------------------------")
+
+#for datatype
+# 
+for i in range(3):
+    print ("meow")
+    
+print("-------------------------------------")
+
+# u can use _ if you dont need that variable
+for _ in range(3):
+    print ("meow")
+    
+#print meow 3 times - one more way to do
+print("meow\n" * 3 , end = "")
