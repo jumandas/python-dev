616e31db68e3ad6db05a54d8589ef3ccca635114
jumandas <dasjuman9@gmail.com>
2025-01-31 13:35:12 +0530

    using functions- def for while and for loop

--- cat.py
+++ cat.py
@@ -36,3 +36,23 @@ while True:
 for _ in range(n):
     print("meow")
     
+#using functions
+
+def main():
+    n = get_number()
+    meow(n)
+
+def get_number():
+    while True:
+        n = int(input("Whats n?"))
+        if n > 0:
+            break
+    return n
+    
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+        
+        
+main()
