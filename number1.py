912bed75a22e3cb8a04fe5f39517073dfac8c98a
jumandas <dasjuman9@gmail.com>
2025-02-03 22:13:24 +0530

    using pass keyword

--- number1.py
+++ number1.py
@@ -12,4 +12,21 @@ def get_int():
         else:
             return x
     
+#main()
+
+
+#pass - means ignoring exception
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            x = int(input("Whats x? "))
+        except ValueError:
+            pass
+        else:
+            return x
+    
 main()
