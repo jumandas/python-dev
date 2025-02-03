91075c4fd6e7e7648b9f92460b1afaab46705c6f
jumandas <dasjuman9@gmail.com>
2025-02-03 21:58:33 +0530

    exception handling with loops

---
+++ number1.py
@@ -0,0 +1,15 @@
+#exception handling with loops 
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            x = int(input("Whats x? "))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            return x
+    
+main()
