86e6d0ad6415e0baac1ad0f6a76c20e9b875f81a
jumandas <64024146+jumandas@users.noreply.github.com>
2025-02-04 16:21:19 +0530

    Random Module Python

--- generate.py
+++ generate.py
@@ -1,9 +1,22 @@
 #modules 
-#random module
-
-#give random heads and tails
+#random module ( to generate random number/choice and help shuffle a list)
 
 import random 
 
+#use from to import a particular module into a variable
+#from random import choice
+#coin = choice(["heads","tails"])
+
+#provide a random choice
 coin = random.choice(["heads","tails"])
 print(coin)
+
+#generate a random num between a range
+number = random.randint(1,10)
+print(number)
+
+#shuffle a list
+cards = ["jack","queen","king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
