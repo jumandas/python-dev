dff4013b737b9613505300b25b77fdf2cde38911
jumandas <64024146+jumandas@users.noreply.github.com>
2025-02-04 17:47:37 +0530

    sys argv

--- name.py
+++ name.py
@@ -1,1 +1,12 @@
 #sys stands for system
+#sys.argv (argument vector)
+#passing input while running the argument (command line argument)
+#python name.py Juman
+#sys.argv[0] will print the name of the filte
+#sys.argv[1] will be an input during the command line argument
+
+import sys
+try:
+    print("hello , my name is", sys.argv[1])
+except IndexError:
+    print("too few arguments")
